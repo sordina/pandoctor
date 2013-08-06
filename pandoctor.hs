c214b1d6c4726778b47ea69bd071a82618dabaac
Lyndon Maydwell <lyndon@sordina.net>
2013-08-06 20:50:59 +1000

    1000X speedup

--- pandoctor.hs
+++ pandoctor.hs
@@ -10,32 +10,38 @@ import System.Posix.Env
 import Control.Arrow
 
 main :: IO ()
-main = do
-  counterIO <- newIORef 0
-  getContents >>= return . readMarkdown def >>= bottomUpM (process counterIO) >> return ()
+main = do counter <- newIORef 0
+          getContents >>= return . readMarkdown def
+                      >>= bottomUpM (process counter) >> return ()
 
 process :: (IORef Int) -> Block -> IO Block
-process counterIO cb@(CodeBlock trip@(_id, _classes, namevals) contents) = do
+process counter cb@(CodeBlock trip@(_id, _classes, namevals) contents) = do
   case lookup "data-filter" namevals
-       of     Just x -> (bump counterIO)
-                     >> CodeBlock trip `fmap` (comeIn counterIO x contents namevals)
+       of     Just x -> CodeBlock trip `fmap` (comeIn counter x contents namevals)
               _      -> return $ cb
 process _ x = return x
 
 comeIn :: IORef Int -> String -> String -> [(String,String)] -> IO String
-comeIn counterIO command input namevals = do
-  count <- readIORef counterIO
+comeIn counter command input namevals = do
+  count <- bump counter
+
   let withCount  = ("PANDOCTOR_COUNT", show count) : namevals
       fixedNames = map (first (map underscore)) withCount
       pairs      = map equalize fixedNames
+
   mapM_ putEnv pairs
+
   (hin,hout,herr,_pid) <- runInteractiveProcess command pairs Nothing Nothing
-  hSetBuffering hin NoBuffering
+
   hPutStrLn hin input
+  hClose hin
+
   out    <- hGetContents hout
   outErr <- hGetContents herr
+
   putStr out
   putStr outErr
+
   return (out ++ "\n" ++ outErr)
 
 equalize :: ([Char], [Char]) -> [Char]
@@ -45,5 +51,5 @@ underscore :: Char -> Char
 underscore x | isAlpha x = x
              | otherwise = '_'
 
-bump :: IORef Int -> IO ()
-bump = flip modifyIORef (+1)
+bump :: IORef Int -> IO Int
+bump r = modifyIORef r (+1) >> readIORef r
